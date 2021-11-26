36af0a5f0f13f00ea9728a3da020370bbb13e1b6
ferri pradana <ferri.pradana@gmail.com>
2021-11-26 17:17:15 +0700

    docker

--- app/main.py
+++ app/main.py
@@ -29,4 +29,8 @@ app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
 
+@app.get("/")
+def root():
+    return {"detail":"hello world man"}
+
 add_pagination(app)
